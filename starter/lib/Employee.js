364965b66018e513fa3ad1447c028c9c37f04f2e
Isabel Solana Santamaria <mariaisabelsolanasantamaria.gmail.com>
2024-02-13 17:15:37 +0000

    adding validation user input

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -2,6 +2,20 @@
 class Employee {
     constructor(name,id,email,role= "Employee")
     {
+
+      // Check if 'name' is a string and is non empty
+      if (typeof name !== "string" || name.trim() === "") {
+        throw new Error("Input invalid! Enter a string as a name");
+      }
+      // Check if 'id' is a positive number (integer)
+      if (!Number.isInteger(id) && id <= 0) {
+        throw new Error("Input invalid! Enter a positive integer as id");
+      }
+      // Check if email has a correct format
+      if (!/^\S+@\S+\.\S+$/.test(email)) {
+        throw new Error("Invalid e-mail format");
+      }
+
         this.name = name;
         this.id = id;
         this.email = email;
